fa01de940cbfa01481ecbd69dfeb18c2e37af566
@AOY <phattaraporn.n1994@gmail.com>
2025-11-08 19:57:48 +0700

    Create integration.ts

---
+++ src/lib/integration.ts
@@ -0,0 +1,14 @@
+// src/lib/integration.ts
+import * as geotab from "./services/geotab";
+import * as route4me from "./services/route4me";
+import * as descartes from "./services/descartes";
+import * as elite from "./services/eliteExtra";
+import * as appian from "./services/appian";
+
+export const Integration = {
+  geotab,
+  route4me,
+  descartes,
+  elite,
+  appian,
+};

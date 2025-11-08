c4503e87021b2a77823be7fa621ed48d8e7afceb
@AOY <phattaraporn.n1994@gmail.com>
2025-11-08 19:57:01 +0700

    Create geotab.ts

---
+++ src/lib/services/geotab.ts
@@ -0,0 +1,10 @@
+// src/lib/services/geotab.ts
+export async function fetchGeotabVehicles() {
+  // TODO: call Geotab API ด้วย GEOTAB_API_KEY
+  return [];
+}
+
+export async function fetchGeotabPositions() {
+  // TODO: ดึงตำแหน่ง real-time
+  return [];
+}
